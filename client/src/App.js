0bd9bbfae024fbd9c1cff58c2ba2dbff6bd036f3
utkarshm2601 <saumyachs12@gmail.com>
2024-01-31 09:06:37 +0530

    Added client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PagenotFound from "./pages/PagenotFound";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PagenotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
