5d977a77f2594ade1c2f15d01963283bacf96a5a
SUDH56 <sudhakarsinghchauhan56@gmail.com>
2024-02-02 20:02:36 +0530

    Error Fixed

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -17,7 +17,8 @@ const AuthProvider = ({ children }) => {
         token: parseData.token,
       });
     }
-  }, [auth]);
+    //eslint-disable-next-line
+  }, []);
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
